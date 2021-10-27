9424ff35d5fdb555aa3adf23f9a8a378ef9ee721
Rileyd1888 <52174567+Rileyd1888@users.noreply.github.com>
2021-10-27 10:24:39 -0400

    Add files via upload

--- BrickBlock_Group2/src/brickblock_group2/BrickBlock_Group2.java
+++ BrickBlock_Group2/src/brickblock_group2/BrickBlock_Group2.java
@@ -5,6 +5,7 @@
  */
 package brickblock_group2;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 
@@ -19,7 +20,7 @@ public static String personOccupation = "";
 public static double personPhoneNumber = 0; 
 
     public static void main(String[] args) {
-        // TODO code application logic here
+     fullProgramMenu();
     }
  public static void brickArea()
  {
@@ -35,4 +36,81 @@ public static double personPhoneNumber = 0;
        (null, "What is your Phone Number:"));
  
  }
-}
+
+ // Looped menu
+    private static void fullProgramMenu()
+    {
+    String[] choices = {"1. BrickBlock ", "2. Personal Measurements ", 
+                    "3. Price calculator", "4. Quit"};
+
+String menu = "";
+while (!menu.equals("4. Quit")) {
+    menu = (String) JOptionPane.showInputDialog(null, "<html>Choose A Menu Item:<br><br></html>", "Main Menu", 
+                    JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+    if (menu == null) {
+        break; 
+    }
+
+    switch (menu) {
+        case "1. BrickBlock ":
+             project();
+            break;
+        case "2. Personal Measurements ":
+              userinput();
+            break;
+        case "3. Price calculator":
+            calculator();
+            break;
+        case "4. Quit":           
+            break;
+        }
+    }
+    }
+
+    // this is where all the guts of the main project will go
+    private static void project() {
+        //Main project
+        // Must be done first. - main prio
+    }
+
+    //extra, uses user input to calcualte the bricks in a given area
+    private static void userinput() {
+        String firstpoint = ("Make sure you double check your measurements!");
+        JOptionPane.showMessageDialog(null, firstpoint);
+        String secondpoint = ("Go double check your measurements!");
+        
+        
+        int ans = JOptionPane.showOptionDialog(new JFrame(), "Did You double check your measurements? ","Block to Brick calculator",
+         JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+         new Object[] { "Yes", "No" }, JOptionPane.YES_OPTION);
+        switch (ans) {
+            case JOptionPane.YES_OPTION:
+                userMeasurements();
+                break;
+                
+            case JOptionPane.NO_OPTION:
+                JOptionPane.showMessageDialog(null, secondpoint);
+                break;
+                
+            case JOptionPane.CLOSED_OPTION:
+                String ninepoint = ("Opps, you closed the window");
+                JOptionPane.showMessageDialog(null, ninepoint);
+                break;
+                
+            default:
+                break;
+        }
+        
+    }
+    
+    // extra, will ask user for measurements and will calculate like user measurements, but add retail.
+    private static void calculator() {
+        
+    }
+       private static void userMeasurements() {
+        
+    }
+    }
+
+ 
+
